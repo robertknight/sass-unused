cd1b2c50cf7902a4cffb4ee18f1b9f358f7b1db5
Robert Knight <robertknight@gmail.com>
2016-07-18 17:16:36 +0100

    Add some basic tests

---
+++ test/find-unused-test.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const { assert } = require('chai');
+
+const findUnused = require('../lib/find-unused');
+
+describe('findUnused', () => {
+  function test(sass, expectedUnused) {
+    const resolve = _ => sass;
+    assert.deepEqual(findUnused(['test.scss'], resolve), expectedUnused);
+  }
+
+  it('should return unused variables', () => {
+    test(`
+$unused: red;
+    `, ['unused']);
+  });
+
+  it('should not return used variables', () => {
+    test(`
+$used: red;
+.foo { color: $used; }`, []);
+  });
+
+  it('should return unused mixins', () => {
+    test(`@mixin unused { }`, ['unused']);
+  });
+
+  it('should not return used mixins', () => {
+    test(`@mixin used {}; .foo { @include used; }`, []);
+  });
+
+  it('should return unused functions', () => {
+    test(`@function foo();`, ['foo']);
+  });
+
+  it('should not return used functions', () => {
+    test(`@function foo(); .bar { color: foo(); }`, []);
+  });
+});
